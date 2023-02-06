69f3004db2b94ec04fca2fae50b27ac7efc083b0
github_CALISALozan <ozanca1407@gmail.com>
2023-02-06 15:34:11 +0300

    06.02

---
+++ src/test/java/day06_assertions/C02_Assertion.java
@@ -0,0 +1,13 @@
+package day06_assertions;
+
+public class C02_Assertion {
+/*
+    Eger Assert class'i kullanilmazsa
+    JUnit kodlarin sorunsuz olarak calisip bittigini raporlar
+    C01'de testleri if-else ile yaptigimiz icin
+    Testler failed olsa da kodlar sorunsuz calistigi icin
+    testler yesil tik olarak isaretlendi
+
+ */
+
+}
