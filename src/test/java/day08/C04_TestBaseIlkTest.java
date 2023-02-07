b9116f6f76e4be223788721c528359fd405ef083
github_CALISALozan <ozanca1407@gmail.com>
2023-02-07 11:34:55 +0300

    07.02

---
+++ src/test/java/day08/C04_TestBaseIlkTest.java
@@ -0,0 +1,16 @@
+package day08;
+
+import org.junit.Test;
+import utilities.ReusableMethods;
+import utilities.TestBase;
+
+public class C04_TestBaseIlkTest extends TestBase {
+
+    @Test
+    public void test01(){
+    driver.get("https://www.amazon.com");
+        ReusableMethods.bekle(3);
+
+
+    }
+}
